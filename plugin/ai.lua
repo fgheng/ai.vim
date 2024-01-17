25c3ab7e4fff1f92fe5fb7a41b994f50ee15d036
fuguoheng <fuguoheng@kuaishou.com>
2024-01-17 16:56:41 +0800

    chage key map

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -6,7 +6,7 @@ end, {
 })
 
 if not vim.g.ai_no_mappings then
-    vim.api.nvim_set_keymap("n", "<C-a>", ":AI ", { noremap = true })
-    vim.api.nvim_set_keymap("v", "<C-a>", ":AI ", { noremap = true })
-    vim.api.nvim_set_keymap("i", "<C-a>", "<Esc>:AI<CR>a", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>a", ":AI ", { noremap = true })
+    vim.api.nvim_set_keymap("v", "<cr>", ":AI ", { noremap = true })
+    vim.api.nvim_set_keymap("i", "<leader>a", "<Esc>:AI<CR>a", { noremap = true })
 end
