3e402ecd8c7edddba33f0e5886fdaceb762f6edf
fuguoheng <fuguoheng@kuaishou.com>
2024-01-17 19:53:10 +0800

    update

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -7,6 +7,14 @@ end, {
 
 if not vim.g.ai_no_mappings then
     vim.api.nvim_set_keymap("n", "<cr>", ":AI ", { noremap = true })
-    vim.api.nvim_set_keymap("v", "<cr>", ":AI ", { noremap = true })
+    -- vim.api.nvim_set_keymap("v", "<cr>", ":AI ", { noremap = true })
+    vim.keymap.set("v", "<cr>", function ()
+        local buffer = vim.api.get_current_buf()
+        if vim.api.nvim_buf_get_option(buffer, 'buftype') == '' and vim.api.nvim_buf_get_option(buffer, 'modifiable') then
+            vim.api.nvim_command("AI")
+        else
+            return
+        end
+    end, { noremap = true })
     vim.api.nvim_set_keymap("i", "<leader>a", "<Esc>:AI<CR>a", { noremap = true })
 end
