5fbacf866aa113d71753f9ab228fb94e9db59a44
fuguoheng <fuguoheng@kuaishou.com>
2024-01-17 17:36:51 +0800

    debug

--- lua/_ai/commands.lua
+++ lua/_ai/commands.lua
@@ -89,28 +89,27 @@ function M.ai (args)
             }, on_data, on_complete)
         end
     else
-        openai.completions({
-            prompt = prompt,
-        }, on_data, on_complete)
-        -- if prompt == "" then
-        --     -- Insert some text generated using surrounding context
-        --     local prefix = table.concat(vim.api.nvim_buf_get_text(buffer,
-        --         math.max(0, start_row-config.context_before), 0, start_row, start_col, {}), "\n")
-        --
-        --     local line_count = vim.api.nvim_buf_line_count(buffer)
-        --     local suffix = table.concat(vim.api.nvim_buf_get_text(buffer,
-        --         end_row, end_col, math.min(end_row+config.context_after, line_count-1), 99999999, {}), "\n")
-        --
-        --     openai.completions({
-        --         prompt = prefix,
-        --         suffix = suffix,
-        --     }, on_data, on_complete)
-        -- else
-        --     -- Insert some text generated using the given prompt
-        --     openai.completions({
-        --         prompt = prompt,
-        --     }, on_data, on_complete)
-        -- end
+        if prompt == "" then
+            -- Insert some text generated using surrounding context
+            local prefix = table.concat(vim.api.nvim_buf_get_text(buffer,
+                math.max(0, start_row-config.context_before), 0, start_row, start_col, {}), "\n")
+
+            local line_count = vim.api.nvim_buf_line_count(buffer)
+            local suffix = table.concat(vim.api.nvim_buf_get_text(buffer,
+                end_row, end_col, math.min(end_row+config.context_after, line_count-1), 99999999, {}), "\n")
+
+            local cur_line = vim.api.nvim_get_current_line()
+
+            openai.completions({
+                prompt = cur_line,
+                -- suffix = suffix,
+            }, on_data, on_complete)
+        else
+            -- Insert some text generated using the given prompt
+            openai.completions({
+                prompt = prompt,
+            }, on_data, on_complete)
+        end
     end
 end
 
